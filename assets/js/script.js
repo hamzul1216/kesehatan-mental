9ca703b20bd8789cd745d0fc650724bcb859cd9d
Hamzah Hasan <hamzahhasan123@gmail.com>
2025-03-30 23:08:00 +0700

    feat/create all section, add all assets, create blog & psikotes

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,17 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+window.addEventListener("load", function () {
+  const preloader = document.getElementById("preloader");
+
+  // Tambahin delay sebelum mulai fade-out
+  setTimeout(() => {
+    preloader.style.opacity = 0;
+
+    // Tunggu efek fade-out sebelum preloader dihapus
+    setTimeout(() => {
+      preloader.style.display = "none";
+    }, 500); // Sama dengan transition di CSS
+  }, 300); // Delay 1 detik sebelum mulai fade-out
+});
